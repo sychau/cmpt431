2ef1484ef2499b7cc8944cd61e1724a74405e16a
sychau <ls006493@gmail.com>
2024-09-24 23:28:29 -0700

    fix submit

--- submit/a2/submit_area.sh
+++ submit/a2/submit_area.sh
@@ -5,4 +5,4 @@
 #SBATCH --mem=1G
 #SBATCH --partition=slow
 
-srun ./curve_area_parallel --nThreads 8 --nPoints 1000000000 --coeffA 3.2 --coeffB 7.1 --rSeed 37
+srun curve_area_parallel --nThreads 8 --nPoints 1000000000 --coeffA 3.2 --coeffB 7.1 --rSeed 37
--- submit/a2/submit_heat.sh
+++ submit/a2/submit_heat.sh
@@ -5,4 +5,4 @@
 #SBATCH --mem=1G
 #SBATCH --partition=slow
 
-srun ./heat_transfer_parallel --nThreads 4 --gSize 1000 --iCX 0.1 --iCY 0.15 --mTemp 600 --tSteps 4000
+srun heat_transfer_parallel --nThreads 4 --gSize 1000 --iCX 0.1 --iCY 0.15 --mTemp 600 --tSteps 4000
