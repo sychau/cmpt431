dc5d7d79c9b247014ec2e4117b398ad5cb90a357
sychau <ls006493@gmail.com>
2024-10-03 22:19:03 -0700

    a3 script

--- submit/a3/submit.sh
+++ submit/a3/submit.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
+#
+#SBATCH --cpus-per-task=8
+#SBATCH --time=10:00
+#SBATCH --mem=4G
+#SBATCH --partition=slow
 
-# Assign command-line arguments to variables
-executable=$1
-nThreads=$3
-nIterations=$5
-inputFiles=$7
-
-# Display the arguments
-echo "Executable: $executable"
-echo "Number of Threads: $nThreads"
-echo "Number of Iterations: $nIterations"
-echo "Input Files: $inputFiles"
-
-# Submit job with dynamic parameters using sbatch
-sbatch --cpus-per-task=$nThreads \
-       --time=10:00 \
-       --mem=4G \
-       --partition=slow \
-       --wrap="srun $executable --nThreads $nThreads --nIterations $nIterations --inputFile $inputFiles"
+srun page_rank_push --nThreads 8 --nIterations 10 --inputFile input_graphs/roadNet-CA
