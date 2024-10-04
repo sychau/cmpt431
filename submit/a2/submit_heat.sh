3340de542df1be499d9cbc8ef37b5435f1cece04
sychau <ls006493@gmail.com>
2024-10-03 22:14:58 -0700

    a3 script

--- submit/a2/submit_heat.sh
+++ submit/a2/submit_heat.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
+#
 
-# Assign command-line arguments to variables
 executable=$1
 nThreads=$3
 nIterations=$5
 inputFiles=$7
 
 # Display the arguments
-echo "Executable: $executable"
-echo "Number of Threads: $nThreads"
-echo "Number of Iterations: $nIterations"
-echo "Input Files: $inputFiles"
+echo "Argument 1: $arg1"
+echo "Argument 2: $arg2"
 
-# Submit job with dynamic parameters using sbatch
-sbatch --cpus-per-task=$nThreads \
-       --time=10:00 \
-       --mem=4G \
-       --partition=slow \
-       --wrap="srun $executable --nThreads $nThreads --nIterations $nIterations --inputFile $inputFiles"
+#SBATCH --cpus-per-task=$nThreads
+#SBATCH --time=10:00
+#SBATCH --mem=4G
+#SBATCH --partition=slow
+
+srun ./page_rank_pull_parallel --nThreads $nThreads --nIterations $nIterations --inputFile $inputFile
