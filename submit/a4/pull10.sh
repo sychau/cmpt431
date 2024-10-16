718011f65f2d46e0e5e338cfe9309c1fa9c7b7c2
sychau <ls006493@gmail.com>
2024-10-15 22:44:41 -0700

    fix a4 scripts

--- submit/a4/pull10.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --cpus-per-task=8
-#SBATCH --time=10:00
-#SBATCH --mem=4G
-#SBATCH --partition=slow
-
-srun page_rank_pull_parallel --nThreads 8 --nIterations 20 --inputFile input_graphs/test_25M_50M --strategy 4 --granularity 10
--- submit/a4/push.sh
+++ submit/a4/push.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=4G
 #SBATCH --partition=slow
 
-srun page_rank_push_parallel_atomic --nThreads 8 --nIterations 20 --inputFile input_graphs/test_25M_50M --strategy 4 --granularity 10
-srun page_rank_push_parallel_atomic --nThreads 8 --nIterations 20 --inputFile input_graphs/test_25M_50M --strategy 4 --granularity 100
-srun page_rank_push_parallel_atomic --nThreads 8 --nIterations 20 --inputFile input_graphs/test_25M_50M --strategy 4 --granularity 1000
-srun page_rank_push_parallel_atomic --nThreads 8 --nIterations 20 --inputFile input_graphs/test_25M_50M --strategy 4 --granularity 2000
+srun page_rank_push_parallel_atomic --nThreads 8 --nIterations 20 --inputFile /scratch/input_graphs/test_25M_50M --strategy 4 --granularity 10
+srun page_rank_push_parallel_atomic --nThreads 8 --nIterations 20 --inputFile /scratch/input_graphs/test_25M_50M --strategy 4 --granularity 100
+srun page_rank_push_parallel_atomic --nThreads 8 --nIterations 20 --inputFile /scratch/input_graphs/test_25M_50M --strategy 4 --granularity 1000
+srun page_rank_push_parallel_atomic --nThreads 8 --nIterations 20 --inputFile /scratch/input_graphs/test_25M_50M --strategy 4 --granularity 2000
