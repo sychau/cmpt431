f3ca9ed70cf9eb8e919e0726331cf6d89a8c0026
sychau <ls006493@gmail.com>
2024-10-15 22:29:23 -0700

    a4 script

---
+++ submit/a4/pull1000.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#SBATCH --cpus-per-task=8
+#SBATCH --time=10:00
+#SBATCH --mem=4G
+#SBATCH --partition=slow
+
+srun page_rank_pull_parallel --nThreads 8 --nIterations 20 --inputFile input_graphs/test_25M_50M --strategy 4 --granularity 1000
---
+++ submit/a4/push10.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+#SBATCH --cpus-per-task=8
+#SBATCH --time=10:00
+#SBATCH --mem=4G
+#SBATCH --partition=slow
+
+srun page_rank_push_parallel_atomic --nThreads 8 --nIterations 20 --inputFile input_graphs/test_25M_50M --strategy 4 --granularity 10
