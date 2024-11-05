dacd36fe3871a353e855b9e09c9d2ba9bb002feb
sychau <syc54@sfu.ca>
2024-11-05 04:17:26 -0800

    add a6 script

---
+++ submit/a6/heat_2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+#SBATCH --cpus-per-task=1
+#SBATCH --nodes=2
+#SBATCH --ntasks=2
+#SBATCH --partition=slow
+#SBATCH --mem=10G
+
+srun ./heat_transfer_parallel --iCX 0.15 --iCY 0.1 --tSteps 1000 --gSize 4000 --mTemp 600
