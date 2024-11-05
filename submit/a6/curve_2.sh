b19beb2afc4c52ff5dfdad4956a94cd3f86a6b0b
sychau <syc54@sfu.ca>
2024-11-05 05:06:32 -0800

    add a6 script

--- submit/a6/curve_2.sh
+++ submit/a6/curve_2.sh
@@ -4,6 +4,6 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks=2
 #SBATCH --partition=slow
-#SBATCH --mem=10G
+#SBATCH --mem=5G
 
 srun ./curve_area_parallel --coeffA 1.2 --coeffB 0.8 --nPoints 4000000000 --rSeed 129
--- submit/a6/heat_2.sh
+++ submit/a6/heat_2.sh
@@ -4,6 +4,6 @@
 #SBATCH --nodes=2
 #SBATCH --ntasks=2
 #SBATCH --partition=slow
-#SBATCH --mem=10G
+#SBATCH --mem=5G
 
 srun ./heat_transfer_parallel --iCX 0.15 --iCY 0.1 --tSteps 1000 --gSize 4000 --mTemp 600
