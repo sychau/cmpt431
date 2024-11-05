2ee352b447882e776b68724333de4a8fcfb716eb
sychau <syc54@sfu.ca>
2024-11-05 05:10:55 -0800

    add a6 script

--- submit/a6/curve_4.sh
+++ submit/a6/curve_4.sh
@@ -4,6 +4,6 @@
 #SBATCH --nodes=4
 #SBATCH --ntasks=4
 #SBATCH --partition=slow
-#SBATCH --mem=5G
+#SBATCH --mem=2G
 
 srun ./curve_area_parallel --coeffA 1.2 --coeffB 0.8 --nPoints 4000000000 --rSeed 129
