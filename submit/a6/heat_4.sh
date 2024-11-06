47b8b655d686c8588ef35de94f98fee277b551e6
sychau <syc54@sfu.ca>
2024-11-06 03:25:47 -0800

    update a6 script

--- submit/a6/heat_4.sh
+++ submit/a6/heat_4.sh
@@ -4,6 +4,6 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --partition=slow
-#SBATCH --mem=1G
+#SBATCH --mem=10G
 
 srun ./heat_transfer_parallel --iCX 0.15 --iCY 0.1 --tSteps 1000 --gSize 4000 --mTemp 600
